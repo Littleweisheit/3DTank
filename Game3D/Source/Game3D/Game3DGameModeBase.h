4d8e692a9ccd98dc629e61fb1a2da656bfc7cb1d
DESKTOP-8433VK4\xuwei <ghostlovegg@gmail.com>
2017-07-08 23:19:00 +0800

    Start

---
+++ Game3D/Source/Game3D/Game3DGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Game3DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME3D_API AGame3DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
